8b30aa5081032a6c54ede5c0b7cb2c417f8f79f2
jared <jaredkjar@gmail.com>
2022-10-25 17:56:10 -0700

    added better logic

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,27 +1,45 @@
-const { Events } = require('discord.js');
+const { Events } = require('discord.js')
+
+const messageJson = [
+  {
+    match: 'not not',
+    response: 'Nice Try.',
+  },
+  {
+    match: 'not a furry',
+    response: 'Correct!',
+  },
+  {
+    match: 'furry',
+    response: 'He is NOT a furry!',
+  },
+  {
+    match: 'baby',
+    fileResponse: 'https://i.imgur.com/FEfzYwy.png',
+  },
+  {
+    match: 'fortnite',
+    response: 'https://media2.giphy.com/media/RJAjTowsU0K1a/giphy.gif',
+  },
+]
 
 module.exports = {
-	name: Events.MessageCreate,
-	async execute(interaction) {
-		if (interaction.author.bot) return;
-		try {
-            if(interaction?.content?.toLowerCase()?.includes('not not')){
-                await interaction.reply(
-                    `Nice try.`
-                  )
-            }
-            else if(interaction?.content?.toLowerCase()?.includes('not a furry')){
-                await interaction.reply(
-                    `Correct!`
-                  )
-            }
-            else if(interaction?.content?.toLowerCase()?.includes('furry')){
-                await interaction.reply(
-                    `He is NOT a furry!`
-                  )
-            }
-		} catch (error) {
-			console.error(error);
-		}
-	},
-};
+  name: Events.MessageCreate,
+  async execute(interaction) {
+    if (interaction.author.bot) return
+    try {
+      const match = Object.values(messageJson).find((val) =>
+        interaction?.content?.toLowerCase()?.includes(val?.match)
+      )
+      if (match) {
+        match?.fileResponse
+          ? await interaction.reply({
+              files: [match?.fileResponse],
+            })
+          : await interaction.reply(match?.response)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+}
